e570e5e1f316762d5a25a6e718d2a027c60d24c4
TrushBug <73204786+TrushBug@users.noreply.github.com>
2021-05-28 05:04:01 +0200

    Ask for player input and update board

--- TicTacToe/TicTacToe/TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe/TicTacToe/TicTacToe.cpp
@@ -3,26 +3,62 @@
 
 #include <iostream>
 using namespace std;
-char Gameboard[3][3] = {'1', '2', '3', 
-						'4', '5', '6', 
-						'7', '8', '9'};
+char emptySlot = ' ';
+char gameboard[3][3] = { emptySlot, emptySlot, emptySlot,
+						 emptySlot, emptySlot, emptySlot,
+						 emptySlot, emptySlot, emptySlot };
+char player = 'X';
+int turn = 0;
+
 
 void DrawBoard()
 {
+	cout << " ------------- \n";
 	for (int a = 0; a < 3; a++) 
 	{
 		for (int b = 0; b < 3; b++)
-			cout << Gameboard[a][b] << " ";
+			cout << " | " << gameboard[a][b];
 
-		cout << "\n";
+		cout << " | \n ------------- \n";
+	}
+}
+
+
+void AskForPlayerInput() 
+{
+	int input;
+
+	while (true)
+	{
+		cout << "It's " << player << " turn." << " Place your marker at a position between 1 to 9: ";
+		cin >> input;
+
+		int x = (input - 1) / 3;
+		int y = (input - 1) % 3;
+
+
+		if (input >= 1 and input <= 9) 
+			if (gameboard[x][y] == emptySlot)
+			{
+				gameboard[x][y] = player;
+				break;
+			}
+			
 	}
 }
 
 int main()
 {
-	cout << "Tic Tac Toe" << endl;
+	cout << "  Tic Tac Toe" << endl;
 
-	DrawBoard();
+	while (true)
+	{
+		turn++;
+		DrawBoard();
+		AskForPlayerInput();
+
+	}
+
 	system("pause");
 	return 0;
 }
